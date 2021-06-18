22b9723a07db396df3a8037692a827b21129dd7d
AnkitRajCode <rajankit432@gmail.com>
2021-06-18 18:13:19 +0530

    Array, Subarray, 2D Array

---
+++ ApniKasha/Array/2dArray/MatrixMultication.cpp
@@ -0,0 +1,3 @@
+// n1 x n2 && n2 x n3 
+// n2 is there in both because as we know for multipling 2 matrix:
+// 1st matrix column is equal to 2nd matrix row 
--- ApniKasha/BoilerPlate.cpp
+++ ApniKasha/BoilerPlate.cpp
@@ -44,7 +44,9 @@ int main(){
 }
 
 // boiler plate code for array
+#include <bits/stdc++.h> // when you will use this the no other library is require
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main(){
@@ -58,3 +60,31 @@ int main(){
     }
     return 0;
 }
+
+// 2D Array bolier Plate Code
+#include <bits/stdc++.h> // when you will use this the no other library is require
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+
+    int arr[n][m];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+    return 0;
+}
+
+//output of 2D array
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<m; j++){
+//         cout<<arr[i][j]<<" ";
+//     }
+//     cout<<"endl";
+// }
