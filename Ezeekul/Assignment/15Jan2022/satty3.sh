82872d07a19495f548687510255f685c2e243708
AnkitRajCode <rajankit432@gmail.com>
2022-01-20 01:51:50 +0530

    Assignment

--- Ezeekul/Assignment/15Jan2022/satty3.sh
+++ Ezeekul/Assignment/15Jan2022/satty3.sh
@@ -1,21 +1,13 @@
-while [ 1 ]	
-do
-echo "[start] [stop] [kill] [reload] [restart] [status] [exit]"		
-echo  "Enter your choice "
-read x
-case $x in							
-start)								
-echo  "Starting clock"
-xclock -update 1 
-;;
-stop)							
-echo -n "Stoping clock"
-kill -9 $(pidof xclock)
-;;
-exit)							
-break
-;;
-*) echo "invalid input"
-;;
+echo "Enter the command line args [START] / [STOP]"
+case $1 in
+    "START")
+        echo "Starting..."
+	    xclock &
+        ;;
+    "STOP")
+        echo "Stopped"
+	    kill $(pidof xclock)
+        ;;
+    *) echo "INVALID REQUEST"
+        ;;
 esac
-done
