a37a78f6d7ae3a343cea990380ae3d7a7546ecaf
AnkitRajCode <rajankit432@gmail.com>
2022-02-05 01:27:05 +0530

    typescript

--- TypeScript/src/index.js
+++ TypeScript/src/index.js
@@ -4,24 +4,24 @@ var isWorking = true;
 var x = 'Ankit';
 console.log(id);
 console.log(company);
-//array
+//TODO: Array
 var ids = [1, 2, 3, 4, 5];
 var companies = ['Ankit', 'Raj', 'Raj', 'Raj', 'Raj'];
 console.log(ids);
 console.log(companies);
-//Typle
+//TODO:Typle
 var person = [1, 'Ankit', true];
 console.log(person);
 //Typle Array
 var persons;
 persons = [[1, 'Ankit', true], [2, 'Raj', false]];
 console.log(persons);
-//Union
+//TODO: Union
 var pid;
 pid = 'Ankit'; //string
 //pid = 1; //number
 console.log(pid);
-//Enum
+//TODO: Enum
 var Direction;
 (function (Direction) {
     Direction[Direction["Up"] = 0] = "Up";
@@ -38,3 +38,78 @@ var Direction2;
     Direction2[Direction2["Right"] = 4] = "Right";
 })(Direction2 || (Direction2 = {}));
 console.log(Direction2.Up);
+var Direction3;
+(function (Direction3) {
+    Direction3["Up"] = "UP";
+    Direction3["Down"] = "DOWN";
+    Direction3["Left"] = "LEFT";
+    Direction3["Right"] = "RIGHT";
+})(Direction3 || (Direction3 = {}));
+console.log(Direction3.Left);
+//TODO: Objects
+var Users = {
+    id: 1,
+    name: 'Ankit'
+};
+console.log(Users);
+var Users2 = {
+    id: 68,
+    name: 'Ankit Raj'
+};
+console.log(Users2);
+//TODO: Type Assertion
+var cid = 74;
+console.log(cid);
+// let count = cid as number;
+var count = cid;
+console.log(count);
+//TODO: Function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(8, 4));
+//Void function
+function log(message) {
+    console.log(message);
+}
+log('Ankit');
+log(91);
+var users1 = {
+    id: 100,
+    name: 'Ankit',
+    age: 21
+};
+var users11 = {
+    id: 105,
+    name: 'Ankit'
+};
+users11.id = 108; //agar random value aa raha toh error aayega
+console.log(users1);
+console.log(users11);
+var p1 = 112; //or, const p1:point  = 'Ankit'; 
+// iss k liye interface nahi use kar paye gai,
+// i.e. interface point = number| string
+console.log(p1);
+var adds = function (p, z) { return p + z; };
+var sub = function (p, z) { return p - z; };
+console.log(adds(12, 6));
+console.log(sub(12, 7));
+//TODO: Classes - classes are use to create objects
+var personClass = /** @class */ (function () {
+    function personClass(id, name) {
+        this.id = id; //this is refer to the current object of the class
+        this.name = name;
+    }
+    //we can also create methods inside the class known as member function
+    personClass.prototype.register = function () {
+        return this.id + " " + this.name + " is now registered";
+    };
+    return personClass;
+}());
+var brad = new personClass(140, 'Ankit'); //this is use/trigger to run the constructor
+var mike = new personClass(142, 'Raj');
+// brad.id = 144; //we cannot change the value of id because it is private/protected
+// we also can't do console.log(brad.id);
+brad.name = 'shubham Singh';
+console.log(brad, mike);
+console.log(brad.register());
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -2,16 +2,16 @@ let id:number = 5;
 let company:string = 'Ankit Raj';
 let isWorking:boolean = true;
 let x:any = 'Ankit';
-
 console.log(id);
 console.log(company);
-//array
+
+//TODO: Array
 let ids:number[] = [1,2,3,4,5];
 let companies:string[] = ['Ankit','Raj','Raj','Raj','Raj'];
 console.log(ids);
 console.log(companies);
 
-//Typle
+//TODO:Typle
 let person:[number,string,boolean] = [1,'Ankit',true];
 console.log(person);
 
@@ -20,13 +20,13 @@ let persons:[number,string,boolean][];
 persons= [[1,'Ankit',true],[2,'Raj',false]];
 console.log(persons);
 
-//Union
+//TODO: Union
 let pid:string | number;
 pid = 'Ankit'; //string
 //pid = 1; //number
 console.log(pid);
 
-//Enum
+//TODO: Enum
 enum Direction {
     Up,
     Down,
@@ -41,3 +41,112 @@ enum Direction2 {
     Right
 }
 console.log(Direction2.Up);
+enum Direction3 {
+    Up='UP',
+    Down='DOWN',
+    Left='LEFT',
+    Right='RIGHT'
+}
+console.log(Direction3.Left);
+
+//TODO: Objects
+const Users:{
+    id:number,
+    name:String
+}={
+    id:1,
+    name:'Ankit'
+}
+console.log(Users);
+
+type User={
+    id:number,
+    name:String
+}
+
+const Users2:User={
+    id:68,
+    name:'Ankit Raj'
+}
+console.log(Users2);
+
+//TODO: Type Assertion
+let cid:any = 74;
+console.log(cid);
+// let count = cid as number;
+let count:number = <number>cid;
+console.log(count);
+
+//TODO: Function
+function add(a:number,b:number):number{
+    return a+b;
+}
+console.log(add(8,4));
+
+//Void function
+function log(message: string | number):void{
+    console.log(message);
+}
+log('Ankit');
+log(91);
+
+//TODO: Interface
+interface Userinterface{
+    id:number,
+    name: string,
+    age?: number, //? -> optional bana dega 
+}
+const users1:Userinterface={
+    id:100,
+    name:'Ankit',
+    age:21
+}
+const users11:Userinterface={
+    id:105,
+    name:'Ankit'
+}
+users11.id=108; //agar random value aa raha toh error aayega
+console.log(users1);
+console.log(users11);
+
+type point = number | string
+const p1:point  = 112; //or, const p1:point  = 'Ankit'; 
+// iss k liye interface nahi use kar paye gai,
+// i.e. interface point = number| string
+console.log(p1);
+
+//TODO: INTERFACE WITH FUNCTION
+interface MathFunc{
+    (x:number,y:number):number;
+}
+
+const adds: MathFunc = (p:number,z:number):number => p + z;
+const sub: MathFunc = (p:number,z:number):number => p - z;
+
+console.log(adds(12,6));
+console.log(sub(12,7));
+
+//TODO: Classes - classes are use to create objects
+class personClass {
+    private id: number; // also can do protected
+    name: string;
+
+    constructor(id: number, name: string) { //constructor is use to initialize the values and create the object
+        this.id = id; //this is refer to the current object of the class
+        this.name = name;
+    }
+    
+    //we can also create methods inside the class known as member function
+    register(){
+        return `${this.id} ${this.name} is now registered`;
+    }
+}
+
+const brad = new personClass(140, 'Ankit'); //this is use/trigger to run the constructor
+const mike = new personClass(142, 'Raj'); 
+// brad.id = 144; //we cannot change the value of id because it is private/protected
+// we also can't do console.log(brad.id);
+brad.name = 'shubham Singh'; 
+console.log(brad,mike);
+console.log(brad.register());
+
